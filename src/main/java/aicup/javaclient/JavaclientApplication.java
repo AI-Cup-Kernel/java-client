a3fefa18d58d97dbb11e1f4f03207cc62ad0bd94
alidakik <afdk2003@gmail.com>
2023-08-24 16:15:57 +0330

    Completed login

--- src/main/java/aicup/javaclient/JavaclientApplication.java
+++ src/main/java/aicup/javaclient/JavaclientApplication.java
@@ -3,11 +3,17 @@ package aicup.javaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import aicup.javaclient.control.Initialize;
+
 @SpringBootApplication
 public class JavaclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaclientApplication.class, args);
+		//Initialize init = new Initialize();
+		//init.login();
+
+
 	}
 
 }
--- src/main/java/aicup/javaclient/control/Initialize.java
+++ src/main/java/aicup/javaclient/control/Initialize.java
@@ -6,18 +6,32 @@ import java.security.SecureRandom;
 import java.util.Random;
 import org.json.simple.*;
 import org.json.simple.parser.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
+
+
+@Service
 public class Initialize {
 
 
     
     private int password; // the password that I should recieve from the server to authenticate the server 
-    private int token; // the token that I should send to the server in my requests for authentication
+    private String token; // the token that I should send to the server in my requests for authentication
     private String serverIp; // the ip of the kernal server
     private int serverPort;  // the port of the kernal server
-    private int myPort; // the port that I should run my api on
-    
-    
+    private int myPort; // the port that I should run my api on    
+    private int id; // the player id
+    private final RestTemplate restTemplate;
+    private String url; // the full server url
 
     // a contructor to initialize the needed variables
     public Initialize(){
@@ -26,7 +40,7 @@ public class Initialize {
         
         // init the password
         Random rand = new SecureRandom(); 
-        password = rand.nextInt(1000000000, 999999999);
+        password = rand.nextInt(100000000, 999999999);
 
         // init the server configuration
         JSONParser parser = new JSONParser();
@@ -34,20 +48,62 @@ public class Initialize {
            Object obj = parser.parse(new FileReader("src\\main\\java\\aicup\\javaclient\\config.json"));
            JSONObject jsonObject = (JSONObject)obj;
            serverIp = (String)jsonObject.get("server_ip");
-           serverPort  = (int)jsonObject.get("server_port");
-           
+           serverPort  = ((Long)jsonObject.get("server_port")).intValue();
+           url = "http://" + serverIp +":"+ serverPort;
            
         } catch(Exception e) {
             System.out.println("Error in reading conf.json (the path of the file should be in src\\main\\java\\aicup\\javaclient\\config.json)");
         }
+        
+        this.restTemplate = new RestTemplate();
+
 
 
     }
 
+    
 
- 
+    public void login(){
 
 
+        // TODO: handle the exceptions
 
+        // Constructing the url
+        String path = url + "/login";
+        
+        // Create form data using MultiValueMap
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("token", Integer.toString(password));
 
+        // Request headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(formData, headers);
+        
+        // Request
+        try{
+            ResponseEntity<String> response = restTemplate.postForEntity(path, requestEntity, String.class);
+        
+            // Reading the response
+            JSONParser parser = new JSONParser();
+            try {
+                JSONObject jsonResponse = (JSONObject) parser.parse(response.getBody());
+                try{
+                    token = (String)jsonResponse.get("token");
+                    id = ((Long) jsonResponse.get("player_id")).intValue();
+                    myPort = ((Long) jsonResponse.get("port")).intValue();;
+                }catch (Exception e){
+                    System.out.println("there is a problem in the server response");
+                    System.out.println((String) jsonResponse.get("error"));
+                }
+            } catch (ParseException e) {
+                System.out.println("there is a problem in the server response");
+                System.out.println("there is no error message in the response");
+            }
+        }catch (ResourceAccessException e){
+            System.out.println("Connection Refused or Unavailable: " + e.getMessage());
+        
+        }
+
+    }
 }
