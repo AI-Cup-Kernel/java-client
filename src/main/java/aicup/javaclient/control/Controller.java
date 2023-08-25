7c1f19f0b0aba49eda01256b1051506b9ab380b4
alidakik <afdk2003@gmail.com>
2023-08-25 21:30:21 +0330

    Client api

--- src/main/java/aicup/javaclient/control/Controller.java
+++ src/main/java/aicup/javaclient/control/Controller.java
@@ -1,65 +1,88 @@
 package aicup.javaclient.control;
 
-import java.util.Map;
 
+
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import aicup.javaclient.PlayerCode;
 
 
 @RestController
 public class Controller {
     
     private Game game;
+    private Initialize init;
 
     public Controller(){
         game = new Game();
+        init = Initialize.getInstance();
     }
     
     
     
     @GetMapping("/init")
-    public String initializer(){
-        System.out.println("test 1 competed");
-        System.out.println(game.getNumberOfTroopsToPut());
-        /*Map<Integer,Integer> m =  game.getStrategicNodes();
-        for (Integer name: m.keySet()) {
-            String key = name.toString();
-            String value = m.get(name).toString();
-            System.out.println(key + " " + value);
-        }*/
-        return "ok";
+    public ResponseEntity<String> initializer(@RequestHeader("x-access-token") String tokenFromHeader){
+        game.setMyTurn(true);
+        System.out.println("initializer started");
+        Thread thread = new Thread(() -> {
+            PlayerCode.initializer(game); 
+        });
+        thread.start(); 
+        return validateRequest(tokenFromHeader);
+
     }
 
     @GetMapping("/turn")
-    public String turn(){
+    public ResponseEntity<String> turn(@RequestHeader("x-access-token") String tokenFromHeader){
+        game.setMyTurn(true);
+        System.out.println("turn started");
+        Thread thread = new Thread(() -> {
+            PlayerCode.turn(game); 
+        });
+        thread.start();
+        return validateRequest(tokenFromHeader);
 
-        System.out.println("test 2 competed");
-        return "ok";
     }
 
     
     @GetMapping("/end")
-    public String endTurn(){
-        System.out.println("test 3 competed");
-        return "ok";
+    public ResponseEntity<String> endTurn(@RequestHeader("x-access-token") String tokenFromHeader){
+        game.setMyTurn(false);
+        System.out.println("turn ended");
+        return validateRequest(tokenFromHeader);
     }
 
     
     @GetMapping("/kill")
-    public String shutdown(){
-        System.out.println("test 4 competed");
-        return "ok";
+    public ResponseEntity<String> shutdown(@RequestHeader("x-access-token") String tokenFromHeader){
+        System.out.println("kill");
+        System.exit(0);
+        return validateRequest(tokenFromHeader);
+
     }
 
     
+
 
 
 
 
     // a function to validate the token in the server requests
-    private boolean checkToken(){
+    private ResponseEntity<String> validateRequest(String tokenFromHeader){
 
-        return true;
+        if (tokenFromHeader.equals(init.getToken())) {
+            return ResponseEntity.ok("ok");
+        }
+        JSONObject errorObject = new JSONObject();
+        errorObject.put("error", "Invalid token!");
+        String errorMessage = errorObject.toJSONString();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
+
     }
 
 
