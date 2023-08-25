647a89bdc082cca570d9e1ea5c1fa2938066c793
alidakik <afdk2003@gmail.com>
2023-08-25 21:11:22 +0330

    adding PlayerCode class

---
+++ src/main/java/aicup/javaclient/PlayerCode.java
@@ -0,0 +1,17 @@
+package aicup.javaclient;
+
+import aicup.javaclient.control.Game;
+
+public class PlayerCode {
+
+
+
+    public static void initializer(Game game){
+
+    }
+
+    public static void turn(Game game){
+
+    }
+    
+}
